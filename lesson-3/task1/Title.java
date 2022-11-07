3f09dc39e6459983978b7c3077f06350cc9117d6
Varvarapwd <115477391+Varvarapwd@users.noreply.github.com>
2022-11-07 16:40:14 +0100

    Create Title.java

---
+++ lesson-3/task1/Title.java
@@ -0,0 +1,13 @@
+public class Title {
+    private final String polishTitle;
+    private final String englishTitle;
+
+    Title(String polishTitle, String englishTitle) {
+        this.polishTitle = polishTitle;
+        this.englishTitle = englishTitle;
+    }
+
+    public String title(String langIsoCode) {
+        return langIsoCode.equals("ENG") ? this.englishTitle : this.polishTitle;
+    }
+}
