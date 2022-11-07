e6185287bfcce293fab07bbf634a0c6896ad81f5
Varvarapwd <115477391+Varvarapwd@users.noreply.github.com>
2022-11-07 16:39:23 +0100

    Create Type.java

---
+++ lesson-3/task1/Type.java
@@ -0,0 +1,11 @@
+public class Type {
+    private final String name;
+
+    Type(String name) {
+        this.name = name;
+    }
+
+    public String typesName() {
+        return this.name;
+    }
+}
