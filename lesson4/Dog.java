b5ff77cc9f3e3ac6dc6a1af54b323bfc28c3d34e
Varvarapwd <115477391+Varvarapwd@users.noreply.github.com>
2022-11-14 16:38:10 +0100

    Create Dog.java

---
+++ lesson4/Dog.java
@@ -0,0 +1,22 @@
+public class Dog implements Animal {
+
+    public void animalSound() {
+
+        System.out.println("The dog says: bow wow");
+    }
+
+    public void sleep() {
+
+        System.out.println("Zzz.Your dog is sleeping right now");
+
+    }
+
+    public void jump() {
+        System.out.println("Ops. I'm sorry, I accidentally broke your vase...:/ ");
+    }
+
+    public void swim() {
+        System.out.println("WOOOW. I love swimming");
+    }
+
+}
