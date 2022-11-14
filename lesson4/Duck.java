0ae3ae3238cc44f1dc791b224531a6b6e9ba411e
Varvarapwd <115477391+Varvarapwd@users.noreply.github.com>
2022-11-14 16:38:49 +0100

    Create Duck.java

---
+++ lesson4/Duck.java
@@ -0,0 +1,24 @@
+public class Duck implements Animal {
+    //private final float
+    public void animalSound() {
+
+        System.out.println("The duck says: quack quack");
+    }
+
+    //@Override
+    public void sleep() {
+
+        System.out.println("Zzz");
+
+    }
+
+    // @Override
+    public void jump() {
+        System.out.println("I can jump");
+    }
+
+    public void swim() {
+        System.out.println("I love swimming. It's my nature");
+    }
+
+}
