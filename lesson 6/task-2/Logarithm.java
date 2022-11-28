a62998db50f32be6e094860baf96495502f8df3b
Varvarapwd <115477391+Varvarapwd@users.noreply.github.com>
2022-11-28 16:42:30 +0100

    Update Logarithm.java

--- lesson 6/task-2/Logarithm.java
+++ lesson 6/task-2/Logarithm.java
@@ -3,21 +3,23 @@ public class Logarithm implements Number {
     private double base, argument;
 
     @Override
-    public double doubleValue() {
-        double value;
-        value =  Math.log(this.argument) / Math.log(this.base);
-        System.out.printf("Argument:\t Base:\t Answer:\n",argument,base,value);
-        return value;
-
+    public double doubleValue() throws(ArithmeticException){
+        if (base <= 0 || base == 1 || argument <= 0) {
+            throw new ArithmeticException("Error");
+        } else {
+            return Math.log(this.argument) / Math.log(this.base);
+            
+        }
     }
+    
 
     public Logarithm(double inputBase, double inputArgument) {
-        try {
+       
             this.base = inputBase;
             this.argument = inputArgument;
 
-        } catch (Exception e) {
+        
             System.out.println("Something went wrong.");
-        }
+        
     }
 }
