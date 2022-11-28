0c5d86efb34938a88331570556a179cd822f5ef7
Varvarapwd <115477391+Varvarapwd@users.noreply.github.com>
2022-11-28 16:07:03 +0100

    Delete Main.java

--- lesson 6/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Calculations calculate = new Calculations();
-        calculate.max(4,6);
-        calculate.min(5,8);
-        calculate.avg(7,9);
-
-    }
-}
