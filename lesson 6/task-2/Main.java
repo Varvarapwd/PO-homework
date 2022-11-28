90df9d77065fb81b5d8e618c796a0f75c9dc991f
Varvarapwd <115477391+Varvarapwd@users.noreply.github.com>
2022-11-28 16:34:10 +0100

    Create Main.java

---
+++ lesson 6/task-2/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Logarithm log = new Logarithm(6, 6);
+        }
+        catch(Exception e) {
+            System.out.println("Something went wrong.");
+        }
+
+    }
+}
