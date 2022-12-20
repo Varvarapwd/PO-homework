b1468e1739b85e1f245c322f2f0055b493e9f1d7
Varvarapwd <115477391+Varvarapwd@users.noreply.github.com>
2022-12-20 17:28:41 +0100

    Update Main.java

--- lesson-4/Main.java
+++ lesson-4/Main.java
@@ -1,19 +1,19 @@
 class Main {
   public static void main(String[] args) {
     Dog myDog = new Dog();
-    myDog.animalSound();
+    myDog.giveSound();
     myDog.sleep();
     myDog.jump();
     myDog.swim();
     System.out.println("");
     Duck myDuck = new Duck();
-    myDuck.animalSound();
+    myDuck.giveSound();
     myDuck.sleep();
     myDuck.jump();
     myDuck.swim();
     System.out.println("");
     Cat myCat = new Cat();
-    myCat.animalSound();
+    myCat.giveSound();
     myCat.sleep();
     myCat.jump();
     myCat.swim();
